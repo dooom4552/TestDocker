7aa2db6c6b4012575d5a5141eaf60a56215d227c
Рита Миннекаева <RitaRita1957@hotmail.com>
2020-08-06 23:08:03 +0300

    работа с брэнд коллектино

---
+++ TestDocker/TestDocker/ViewsModels/BrandCollectionBrandNameViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestDocker.ViewsModels
+{
+    public class BrandCollectionBrandNameViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BrandName { get; set; }
+        public int BrandId { get; set; }
+    }
+}
